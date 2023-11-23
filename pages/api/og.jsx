4d8c7356bec4ad0dc867d3fb02d14fe9c7fac865
이성인 <devlsi1228@gmail.com>
2023-11-24 03:20:50 +0900

    test

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -7,9 +7,6 @@ export const config = {
 };
 
 export default async function handler() {
-	const imageData = await fetch(
-		new URL("./landing-bear.png", import.meta.url)
-	).then((res) => res.arrayBuffer());
 	return new ImageResponse(
 		(
 			<div
@@ -28,12 +25,11 @@ export default async function handler() {
 					style={{
 						display: "flex",
 						color: "#000000",
-						fontSize: 80,
+						fontSize: 100,
 					}}
 				>
 					곰터뷰 ⭐️
 				</div>
-				<img width="300" height="300" src={imageData} />
 			</div>
 		),
 		{
